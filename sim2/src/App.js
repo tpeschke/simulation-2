8f8e414defd67c54463c48ab9164dec8d76a4844
Trent <mr.peschke@gmail.com>
2017-12-22 16:50:35 -0700

    finish push

--- sim2/src/App.js
+++ sim2/src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import axios from 'axios'
+
 import AddNewS1 from './componets/AddNewS1'
+
+
 
 class App extends Component {
   render() {
